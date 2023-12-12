e7d404b5c7f1bf718470d89229b607006c2878b9
ngocngotho <ngocngotho@tcom.vn>
2023-12-12 21:53:03 +0700

    log cpu

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const app = require('./src/app')
 
 const PORT = process.env.PORT || 3636
-
+const { cpus } = require('os');
 const server = app.listen(PORT,()=>{
+    console.log('---------------CPUCORE-------------',cpus(),cpus().length)
     console.log(`WSV eCOMERCE start with ${PORT}`)
 })
 
